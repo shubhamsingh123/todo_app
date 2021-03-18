ca9bc19601f5c2cc924fd1fd2bad743e42378e25
Shubham Singh <Shubham16cse06@gmail.com>
2021-03-18 13:14:53 +0530

    Add input + button

--- src/App.js
+++ src/App.js
@@ -1,7 +1,20 @@
-import React from "react";
+import React, { useState } from "react";
 
 function App() {
-  return <div className="app"></div>;
+  const counting = 1 + 0;
+  return (
+    <div className="app">
+      <h1>TODO: {counting}</h1>
+      <input type="text" />
+      <button>Add TODO</button>
+
+      <ul>
+        <li>Take dog for a walk</li>
+        <li>Take a rubbish out</li>
+        <li>Study Time</li>
+      </ul>
+    </div>
+  );
 }
 
 export default App;
